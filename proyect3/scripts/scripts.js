ae8fafc01278c942aeb913729fc6e0ee3e414727
JesusRRr <rolonjesus1996@gmail.com>
2019-08-09 23:31:47 -0500

    different colors were added to the progress bar

--- proyect3/scripts/scripts.js
+++ proyect3/scripts/scripts.js
@@ -2,12 +2,20 @@ const app=new Vue({
     el: "#app",
     data:{
         mensaje: 'Hello',
-        contador:0
+        contador:0,
+
     },
     
     computed:{
         invertido(){
             return this.mensaje.split('').reverse().join('');
+        },
+        color(){
+            return{
+                'bg-success' : this.contador <= 10,
+                'bg-warning'  : this.contador >10 && this.contador <= 20,
+                'bg-danger'  : this.contador >20 
+            }
         }
     }
 
