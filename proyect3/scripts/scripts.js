27cfdfeb2d74b5c8915ce4ddddf520eb7df037a5
JesusRRr <rolonjesus1996@gmail.com>
2019-08-09 23:04:38 -0500

    add invertido fuction computed to invert a message

--- proyect3/scripts/scripts.js
+++ proyect3/scripts/scripts.js
@@ -1,7 +1,12 @@
 const app=new Vue({
     el: "#app",
     data:{
-        
+        mensaje: 'Hello'
+    },
+    computed:{
+        invertido(){
+            return this.mensaje.split('').reverse().join('');
+        }
     }
 
 
