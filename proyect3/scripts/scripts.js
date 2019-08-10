3f8a318a8b3bd1862003c8880c5c3f1f05159c83
JesusRRr <rolonjesus1996@gmail.com>
2019-08-09 23:24:15 -0500

    add dynamic progress bar

--- proyect3/scripts/scripts.js
+++ proyect3/scripts/scripts.js
@@ -1,8 +1,10 @@
 const app=new Vue({
     el: "#app",
     data:{
-        mensaje: 'Hello'
+        mensaje: 'Hello',
+        contador:0
     },
+    
     computed:{
         invertido(){
             return this.mensaje.split('').reverse().join('');
