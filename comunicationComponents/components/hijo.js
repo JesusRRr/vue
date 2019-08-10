f90561542083deb3b7cdf1af68807a0212b03dd8
JesusRRr <rolonjesus1996@gmail.com>
2019-08-10 17:17:28 -0500

    passing of data between components

--- comunicationComponents/components/hijo.js
+++ comunicationComponents/components/hijo.js
@@ -4,8 +4,10 @@ Vue.component('hijo',{
     `
         <div class="py-5 bg-warning text-white">
             <h4>Componente hijo</h4>
+            <h5>{{numero}}</h5>
         </div>
     
     
-    `
+    `,
+    props: ['numero']
 })
--- comunicationComponents/components/padre.js
+++ comunicationComponents/components/padre.js
@@ -2,10 +2,16 @@ Vue.component('padre',{
     template: //html
     `
     <div class="p-5 bg-dark text-white">
-        <h2>Componente padre</h2>
-        <hijo></hijo>
+        <h2>Componente padre: {{numeroPadre}}</h2>
+       <button class="bt btn-danger" @click="numeroPadre++">+</button>
+        <hijo :numero="numeroPadre"></hijo>
     
     </div>   
     
-    `
+    `,
+    data(){
+        return {
+            numeroPadre: 0
+        }
+    }
 })
