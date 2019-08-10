3076039f016cb5959117261afeb18bd96a848a1d
JesusRRr <rolonjesus1996@gmail.com>
2019-08-10 16:38:52 -0500

    second component added

--- components/components/saludo.js
+++ components/components/saludo.js
@@ -1,5 +1,11 @@
 Vue.component('saludo',{
-    template:'<h1>{{saludo}}</h1>',
+    template:`
+        <div> 
+            <h1>{{saludo}}</h1>
+            <h3>Hey</h3>
+        </div> 
+    `,
+    
     data(){
         return{
             saludo:'Hola desde Vue'
